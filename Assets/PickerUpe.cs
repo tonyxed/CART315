798e01392d46788e28927aebf75ca30e30b34508
Anthony Calderone <89947042+tonyxed@users.noreply.github.com>
2023-02-10 11:11:37 -0500

    Tutorial4Finished

--- Assets/PickerUpe.cs
+++ Assets/PickerUpe.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickerUpe : MonoBehaviour
 {
     public int count = 0;
+    public GameObject particlePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,8 @@ public class PickerUpe : MonoBehaviour
     {
         if (collision.collider.gameObject.CompareTag("pickUp")) //will destroy on pickup
         {
+            GameObject.Instantiate(particlePrefab, collision.collider.gameObject.transform.position, Quaternion.identity);
+
             GameObject.Destroy(collision.collider.gameObject);
             count = count + 1;
         }
