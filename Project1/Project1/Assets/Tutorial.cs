eca21db252e16e42ca97db35ed729c67eb9ddca2
Anthony Calderone <89947042+tonyxed@users.noreply.github.com>
2023-02-15 15:47:53 -0500

    Added Sound Effects

---
+++ Project1/Project1/Assets/Tutorial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI(){
+        GUI.Label (new Rect (0, 0, 200, 200), "Press F for Flashlight!");
+    }
+}
