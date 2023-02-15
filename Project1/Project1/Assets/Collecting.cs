219c4c7dc16fa314248ad22b042f1b83cddab914
Anthony Calderone <89947042+tonyxed@users.noreply.github.com>
2023-02-15 16:51:23 -0500

    Added ability to track papers collected with GUI labels and more sounds

---
+++ Project1/Project1/Assets/Collecting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collecting : MonoBehaviour
+{
+    int someInt = 30;
+    int papers = 0;
+    public AudioSource paperPickUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider Col)
+    {
+        if(Col.gameObject.tag == "Paper")
+        {   
+            Col.gameObject.SetActive(false);
+            papers++;
+            paperPickUp.Play();
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI(){
+        GUI.skin.label.fontSize = someInt;
+        GUI.Label (new Rect (0, 300, 500, 100), "Paper's collected :" + papers + "/3");
+    
+    }
+}
